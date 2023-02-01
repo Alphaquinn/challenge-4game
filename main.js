5211cb41ca53fc2d0449d71d4a8243a939df8676
AlphaQuinn <snowboarderquinn@gmail.com>
2023-02-01 13:55:50 -0500

    still fiddling, need timmer to work and link q's

--- main.js
+++ main.js
@@ -6,36 +6,41 @@ var timerCount = questions.length * 15;
 
 
 
-
+function(){
 function buildQuiz(){
     //variable to store html output
 const outPut = [];
 
 //for each question
 myQuestions.forEach(
-    (currentQuestion,questionNumber=>{
+    (currentQuestion, questionNumber)=>{
 //variable to store the list of possible answers
-const Answers[];
+const answers = [];
 //as well as each available answer
 for(letter in currentQuestion.answers){
 //add html radio button 
 answers.push(
  `<label>
- <input type= "radio" name = Question${questionNumber} "value="${letter}">
+ <input type= "radio" name ="question${questionNumber}"
+value="${letter}">
  ${letter} : 
- $currentQuestion.answers[letter]}
- <label>'
+ ${currentQuestion.answers[letter]}
+ </label>`
  );
 }
 //add this question and its answers to the output
 
 output.push(
-'<div class = "question"> ${currentQuestion.question}</div>
-<div class = "Answers.join('')}</div>'
-)
-)
+    `<div class="slide">
+<div class = "question"> ${currentQuestion.question} </div>
+<div class = "answers"> ${answers.join("")}</div>
+</div>`
+);
 }
-}))
+);
+// combine our output list into one string of HTML to put on the page
+quizContainer.innerHTML = output.join('');
+}
 
 
 
@@ -43,6 +48,68 @@ output.push(
 }
 
 function showResults(){
+    const answerContainers =
+quizContainer.querySelectorAll('.answers');
+
+//tracks answers and tallys score as well as moniters time
+let numCorrect = 0;
+
+myQuestions.forEach((currentQuestion, questionNumber) =>{
+    const answerContainer =
+    answerContainers[questionNumber];
+    const selector =
+    `input[name=question${questionNumber}]:
+    checked`;
+    const userAnwer =
+    answerContainer.querySelector(selector) || {}).value;
+
+
+    //for correct answers
+    if(userAnwer ===currentQuestion.correctAnswer){
+        numCorrect++;
+        answerContainers[questionNumber].style.color = `lightblue`;}
+        else{
+            answerContainer[questionNumber].style.color ='black';}
+        });
+
+        resultsContainer.innerHTML = 
+        `${numCorrect} out of ${myQuestions.length}`;
+        }
+
+        function showSlide(n) {
+            slides[currentSlide].classList.remove(
+                `active-slide`);
+                slides[n].classList.add('active-slide');
+                currentSlide = n;
+                if(currentSlide===0){
+                    previousButton.style.display =
+                    `inline-block`;
+                }
+                if(currentSlide=== slides.length-1){
+                    nextButton.style.display =
+                    `none`;
+                    submitButton.style.display =
+                    `inline-block`;
+                }
+                else{
+                    nextButton.style.display =
+                    `inline-block`;
+                    submitButton.style.display =
+                    `none`;
+                }
+            }
+            function showNextslide() {
+                showSlide(currentSlide+1);
+            }
+            function showPreviousslide(){
+                showSlide(currentSlide-1);
+            }
+            
+            )
+
+        }
+    }
+})
 
 
 
