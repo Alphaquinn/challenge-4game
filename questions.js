0f1fef14a82a72039791ef7b27b8f50b0becab31
AlphaQuinn <snowboarderquinn@gmail.com>
2023-02-01 13:56:24 -0500

    still needs work

--- questions.js
+++ questions.js
@@ -1,41 +1,68 @@
 const myQuestions = [
 
 
-    {
-        Question: " Q1: commonly used data types do not include:",
-        answers: ["A. strings", "B. alerts","C. booleans", "D. numbers"],
-        correctAnswer: "B. alerts"
-    },
+{
+        Question: " commonly used data types do not include:",
+        answers: {
+        A: "strings",
+         B: "alerts",
+         C: "booleans", 
+         D: "numbers"
+        },
+        correctAnswer: "B"
+ },
 
 
-{  Question: "Q2: The condition in an if/else stsatment is enclosed within _______.",
-answers:["A. Parentheses", "B. quotes", "C. curly brackets", "D. square brackets"],
-correctAnswer: "A. parentheses"
+{  Question: "The condition in an if/else statment is enclosed within _______.",
+answers:{
+ A: "Parentheses",
+ B: "quotes",
+ C: "curly brackets",
+ D: "square brackets"
+},
+correctAnswer: "A"
 },
 
 
-{ Question: "Q3: which event occurs when the user clicks on an HTML element?",
-answers: ["A. Onchange", "B. onClick", "C. onmouseOver", "D. onmouseclick"],
-correctAnswer: "B. onClick"
+{ Question: "which event occurs when the user clicks on an HTML element?",
+answers: {
+A: "Onchange", 
+B: "onClick", 
+C: "onmouseOver", 
+D: "onmouseclick"
+},
+correctAnswer: "B"
 },
 
 { Question: "Q4: to see if two variables are equal in an if/else statement you would use ___.",
-answers:[ "A. =","B. ==", "C. equals", "D. !="],
-correctAnswer: "B. =="
+answers:{ 
+A: "=",
+B: "==",
+C:"equals", 
+D:"!="
+},
+correctAnswer: "B"
 
 },
 
 {Question:" Q5: the first index of an array is ____.",
-answers: {"A. 1", 
-B." 0", 
-C. " 8", 
-D. " any"},
-correctAnswer: "B. 0"
+answers: {
+A: "1", 
+B: "0", 
+C: "8", 
+D: "any"
+},
+correctAnswer: "B"
 },
 
 { Question: "Q6: How do you write an if statement in javascript?",
-answers:["A. if i = 6 then", "B. if i ==6 then", "C. if(i==6)", "D. if i = 6"],
-correctAnswer: "C. if(i==6)"
+answers:{
+    A: "if i = 6 then",
+    B: "if i ==6 then", 
+     C: "if(i==6)", 
+     D: "if i = 6"
+    },
+correctAnswer: "C"
 
 
 }
